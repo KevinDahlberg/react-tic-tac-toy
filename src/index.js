1544f0f56142bbaf653f0d3036b14308488d2b1a
Kevin Dahlberg <nsu_tpt@hotmail.com>
2017-06-03 20:20:51 -0500

    tried rendering with a for loop, didn't work.  Have to use map instead

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ function Square(props){
   }
 
 class Board extends React.Component {
+
   renderSquare(i) {
     return (
             <Square
@@ -21,24 +22,36 @@ class Board extends React.Component {
   }
 
   render() {
+    const boardRows = 3;
+    const boardColumns = 3;
     return (
-       <div>
+      <div>
+      for (let i = 0; i < boardRows; i++) {
         <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
+        for (let j = 0; j < boardColumns; j++) {
+          let squareValue = i;
+          {this.renderSquare(squareValue)}
+        }
         </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-          </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-          </div>
-          </div>
+      }
+      </div>
+      //  <div>
+      //   <div className="board-row">
+      //     {this.renderSquare(0)}
+      //     {this.renderSquare(1)}
+      //     {this.renderSquare(2)}
+      //   </div>
+      //   <div className="board-row">
+      //     {this.renderSquare(3)}
+      //     {this.renderSquare(4)}
+      //     {this.renderSquare(5)}
+      //     </div>
+      //   <div className="board-row">
+      //     {this.renderSquare(6)}
+      //     {this.renderSquare(7)}
+      //     {this.renderSquare(8)}
+      //     </div>
+      //     </div>
     );
   }
 }
